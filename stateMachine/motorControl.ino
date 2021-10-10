ecd336889b48432f672cb7bfb4178f45a074204e
Fit-Kisto <89818151+Fit-Kisto@users.noreply.github.com>
2021-10-10 19:59:38 +1100

    changed to use parameters passed

--- stateMachine/motorControl.ino
+++ stateMachine/motorControl.ino
@@ -1,30 +1,24 @@
 void motorControl(uint8_t xDirR, uint8_t xMagnitudeR, uint8_t yDirR, uint8_t yMagnitudeR)
 {
   // This code is used when controlling the drive motors
-  if (Serial.available() > 0) {
-    char instructions = Serial.read();
-    if (instructions == 'a') {
-      Speed = Serial.parseInt();
+  if (Serial.available() > 0) 
+    if (xDirR == 0) {
+      Speed = 1500 + (2*xMagnitudeR);
       leftDriveSpeed = rightDriveSpeed = Speed;
     }
-    else {
-      char lr = Serial.read();
-      Speed = Serial.parseInt();
-      if (instructions == 'b') {
-        if (lr == 'l') {
-          leftDriveSpeed = Speed;
-        }
-        else {
-          rightDriveSpeed = Speed;
-        }
+    else if (xDirR == 1) {
+      Speed = 1500 - (2*xMagnitudeR);
+      leftDriveSpeed = rightDriveSpeed = Speed;
+    }
+    if (xMagnitudeR == 0) {
+      Speed = 1500 + (2*yMagnitudeR);
+      if (yDirR == 0) {
+        leftDriveSpeed = Speed;
+        rightDriveSpeed = 2510 - Speed;
       }
-      else {
-        if (lr == 'l') {
-          leftDriveSpeed = Speed;
-        }
-        else {
-          rightDriveSpeed = Speed;
-        }
+      else if (xMagnitude == 1){
+        leftDriveSpeed = Speed;
+        rightDriveSpeed = Speed;
       }
     }
   
